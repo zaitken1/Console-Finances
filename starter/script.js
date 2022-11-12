fbadd6a29a8a0760e042ae48ddcc8322c7467cd2
zaitken <zaraaitken18@gmail.com>
2022-11-12 14:27:38 +0000

    initial commit - adds heading and total number on months

---
+++ starter/script.js
@@ -0,0 +1,10 @@
+// Add heading Financial Analysis
+{
+    document.write("Financial Analysis<br>-------------------------<br>");
+}
+
+// Log total number of months in data
+{
+    document.write("Total Months: " + finances.length)
+}
+
