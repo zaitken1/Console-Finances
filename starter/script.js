38cfd2d6c5cac23bbbe8cf4a738990bede008bbe
zaitken <zaraaitken18@gmail.com>
2022-11-13 16:44:23 +0000

    Adds total value, average value & rounds 2 decimal places

--- starter/script.js
+++ starter/script.js
@@ -10,7 +10,32 @@
 
 // Total amount of profit/losses
 
-for ( var j=0; j < finances.length; j++ )
-  {
-     document.write("<br>Total: $", finances[j][1]);
-  }
+let sum=0;
+
+for(let i=0; i<finances.length; i++){
+    sum=sum+finances[i][1]
+}
+document.write("<br>Total: $", sum)
+
+// The average of the changes in Profit/Losses over the entire period
+
+let differenceArray = []
+
+for (let i=0; i<finances.length-1; i++){
+    let initialMonth = finances[i];
+    let followingMonth = finances[i+1];
+    let difference = followingMonth[1]-initialMonth[1]
+    differenceArray.push(difference)
+}
+
+let totalValue = differenceArray.reduce(function(a,b){
+    return a+b
+
+})
+
+let finalValue = totalValue/finances.length
+
+let roundedValue = finalValue.toFixed(2)
+
+document.write("<br>Average Change $", roundedValue)
+
